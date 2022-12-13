27ce12835d0fd17a6467975d59d7a52c09a9b05d
n-vgh98 <nazi.gh988@gmail.com>
2022-12-13 13:18:04 +0330

    Add serializers.py in users and create UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework import generics
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
