361b8cb44cbbeeb075e0d57970ff18cb852e6c0c
n-vgh98 <nazi.gh988@gmail.com>
2022-12-13 12:46:53 +0330

    Add User and Nationality models

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from decouple import config
 
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = 'static/'
 
 # Default primary key field type
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Nationality(models.Model):
+    country = models.CharField(max_length=64)
+    nationality = models.CharField(max_length=64)
+    language = models.CharField(max_length=64)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+class User(AbstractUser):
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, 'male'),
+        (FEMALE, 'female')
+    )
+    phone_number = models.CharField(unique=True, max_length=12)
+    nationality = models.ForeignKey(Nationality, related_name='users_nationality', null=True, on_delete=models.SET_NULL)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES)
+    avatar = models.ImageField(upload_to='users/avatars/', null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_valid = models.BooleanField(default=True)
+    date_of_join = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
