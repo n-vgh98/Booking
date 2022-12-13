e498a8e6e030f5e88d38934a5d68d1d606b78e9e
n-vgh98 <nazi.gh988@gmail.com>
2022-12-13 13:26:35 +0330

    Add users list url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
-    path('', )
+    path('', views.UserList.as_view(), name='user_list')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from .serializers import *
 from rest_framework import generics
+
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
