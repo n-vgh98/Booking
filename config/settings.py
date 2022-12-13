abb51e25e4da288d323551eb65971dd1df12c5c5
n-vgh98 <nazi.gh988@gmail.com>
2022-12-13 13:01:22 +0330

    Change Auth user model in settings

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #third_apps
-    'rest_framework'
+    'rest_framework',
 
     #my_apps
     'users',
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
+
+
+class UserManager(UserManager):
+    pass
 
 class Nationality(models.Model):
     country = models.CharField(max_length=64)
@@ -20,9 +24,13 @@ class User(AbstractUser):
     )
     phone_number = models.CharField(unique=True, max_length=12)
     nationality = models.ForeignKey(Nationality, related_name='users_nationality', null=True, on_delete=models.SET_NULL)
-    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, null=True)
     avatar = models.ImageField(upload_to='users/avatars/', null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     is_valid = models.BooleanField(default=True)
     date_of_join = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
