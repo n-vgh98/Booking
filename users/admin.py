4bcdfdc32ca4f56d5eaacc6923c21cc5ad829ce2
n-vgh98 <nazi.gh988@gmail.com>
2022-12-13 13:08:49 +0330

    Customize user admin and nationality admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Nationality)
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ('id', 'username', 'date_of_join', 'is_valid')
+    search_fields = ('first_name', 'last_name', 'username')
+    list_filter = ('is_valid',)
+
+
+class AdminNationality(admin.ModelAdmin):
+    list_display = ('id', 'country', 'nationality', 'language', 'is_valid')
+    search_fields = ('country', 'nationality', 'language')
+    list_filter = ('is_valid',)
+
+
+admin.site.register(User, AdminUser)
+admin.site.register(Nationality, AdminNationality)
