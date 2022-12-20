57264b4e8c03473935cdf9ce3e1e9ecbdda7e94b
n-vgh98 <nazi.gh988@gmail.com>
2022-12-20 13:37:21 +0330

    Add Hotel and Room and Features of them in hotels/models

---
+++ abstracts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class AbstractFeature(models.Model):
+    title = models.CharField(max_length=128)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+
+class AbstractPlace(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    is_valid = models.BooleanField(default=True)
+    # address = models.Foreignkey(Address)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- hotels/models.py
+++ hotels/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+
+from abstracts.models import AbstractPlace, AbstractFeature
+
+
+class Hotel(AbstractPlace):
+    star = models.PositiveSmallIntegerField(validators=[1 - 5])
+
+    def rate(self):
+        pass
+
+
+class HotelFeature(AbstractFeature):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='hotel_features')
+
+
+class HotelRoom(AbstractPlace):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_nam="hotel_rooms")
+    floor = models.SmallIntegerField()
+    count = models.PositiveSmallIntegerField
+    # day_price = models.ForeignKey()
+    # data_price = models.ForeignKey()
+    extra_bed = models.BooleanField(default=False)
+    breakfast = models.BooleanField(default=True)
+    reserve = models.BooleanField(default=False)
+
+
+class HotelRoomFeature(AbstractFeature):
+    Room = models.ForeignKey(HotelRoom, on_delete=models.CASCADE, related_name="room_features")
