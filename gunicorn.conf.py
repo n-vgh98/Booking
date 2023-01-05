43587dc43560bca409eb23ce0b216e4515dc2d90
n-vgh98 <nazi.gh988@gmail.com>
2023-01-05 18:42:31 +0330

    Add gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+wsgi_app = 'imdb.wsgi:application'
+loglevel = 'debug'
+workers = 2
+bind = '0.0.0.0:8000'
+
+accesslog = '/var/log/gunicorn/booking.log'
+errorlog = '/var/log/gunicorn/booking.log'
+
+capture_output = True
